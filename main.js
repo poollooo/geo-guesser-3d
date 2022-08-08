9a349979a3cf54a93dc93cf85f3282f810d7e159
Paul Nizet <polonizet@gmail.com>
2022-08-08 12:23:27 +0200

    add background stars

--- main.js
+++ main.js
@@ -55,6 +55,27 @@ const group = new THREE.Group()
 group.add(sphere)
 scene.add(group)
 
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+    color: 0xffffff
+})
+
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000
+    const y = (Math.random() - 0.5) * 2000
+    const z = -Math.random() * 3000
+    starVertices.push(x, y, z)
+}
+
+starGeometry.setAttribute(
+    'position',
+    new THREE.Float32BufferAttribute(starVertices, 3)
+)
+
+const stars = new THREE.Points(starGeometry, starMaterial)
+scene.add(stars)
+
 camera.position.z = 14
 
 const mouse = {
