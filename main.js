77a6d2743e90d50ae5889c0ff7d448201a3f3a60
Paul Nizet <polonizet@gmail.com>
2022-08-08 10:04:30 +0200

    sharpen rendering by adding antialias and setting the pixel ratio

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ import * as THREE from 'three'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialias: true
+    }
+)
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 //create sphere
@@ -18,7 +23,7 @@ const sphere = new THREE.Mesh(
 
 console.log('sphere is :', sphere)
 scene.add(sphere)
-camera.position.z = 10
+camera.position.z = 14
 
 function animate() {
     requestAnimationFrame(animate)
