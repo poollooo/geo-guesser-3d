b600e0854583b5b117ca1ef4cd7026271e83396e
Paul Nizet <polonizet@gmail.com>
2022-08-08 10:27:01 +0200

    add fragment shader + setup vite config file

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import * as THREE from 'three'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
+console.log(fragmentShader)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
@@ -16,8 +20,9 @@ document.body.appendChild(renderer.domElement)
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
     // new THREE.MeshBasicMaterial({ color: 0xFF0000 })
-    new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load('./img/globe.jpeg')
+    new THREE.ShaderMaterial({
+        vertexShader,
+        fragmentShader
     })
 )
 
