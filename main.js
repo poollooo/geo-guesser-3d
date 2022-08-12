49d95746573a5af7b0d5b6e0da9a49c255bd6378
Paul Nizet <polonizet@gmail.com>
2022-08-12 12:30:55 +0200

    clean code

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ renderer.setPixelRatio(window.devicePixelRatio)
 //create sphere
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
-    // new THREE.MeshBasicMaterial({ color: 0xFF0000 })
     new THREE.ShaderMaterial({
         vertexShader,
         fragmentShader,
